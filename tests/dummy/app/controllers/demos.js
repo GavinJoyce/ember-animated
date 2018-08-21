3a359d7d38277d2422fc36005311a83e83bfc38a
Gavin Joyce <gavinjoyce@gmail.com>
2018-08-21 13:09:07 -0700

    fixup!

--- tests/dummy/app/controllers/demos.js
+++ tests/dummy/app/controllers/demos.js
@@ -3,7 +3,9 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   actions: {
     changeScale(scaleValue) {
-      let el = document.getElementsByClassName('demos')[0];
+      let el = document.body;
+      // let el = document.getElementsByClassName('demos')[0];
+
       el.style['transform-origin'] = '0px 0px';
       el.style.transform = `scale(${scaleValue})`;
     }
